b0de6fbeb3ddc481107386462bd4049dc71819c6
bk <dubeybk15@gmail.com>
2020-10-31 10:22:32 +0530

    init submit

---
+++ src/main/java/com/monitoring/ui/monitor/MonitoringUiApplication.java
@@ -0,0 +1,13 @@
+package com.monitoring.ui.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringUiApplication.class, args);
+	}
+
+}
